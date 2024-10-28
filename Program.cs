45b5a23a5b95630707b2e818799ff7f2fee494e2
Rory B <rory_bell_88@hotmail.com>
2024-10-28 10:01:49 +1000

    Menu and MenuItem classes

--- Program.cs
+++ Program.cs
@@ -8,8 +8,60 @@ namespace ElevatorChallenge
 {
     class Program
     {
+
+        class MenuItem
+        {
+            public int Index { get; }
+            public string Name { get; }
+
+            public MenuItem(int index, string name)
+            {
+                Index = index;
+                Name = name;
+            }
+
+            public override string ToString()
+            {
+                return
+                    $"Index = {Index}" +
+                    $"\nName = {Name}";
+
+            }
+
+            private void PerformAction() { }
+
+
+        }
+
+        class Menu
+        {
+            private List<MenuItem> menuItemList;
+            public Menu(List<MenuItem> menuItems)
+            {
+                menuItemList = menuItems;
+            }
+
+            public void PrintMenu()
+            {
+                foreach (MenuItem menuItem in menuItemList)
+                {
+                    Console.WriteLine($"{menuItem.Index} {menuItem.Name}");
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            string[] menuItemInputs = { "Up", "Down", "Instructions", "Exit" };
+            List<MenuItem> menuItemsList = new List<MenuItem> { };
+
+            for (int i = 0; i < menuItemInputs.Length; i++)
+            {
+                menuItemsList.Add(new MenuItem(i + 1, menuItemInputs[i]));
+            }
+
+            Menu mainMenu = new Menu(menuItemsList);
+            mainMenu.PrintMenu();
+            Console.Read();
         }
     }
 }
